13eca075dd33f5d625df4e8dbea6e7934c1ba8a1
eduardo moreno <emoreno@balam>
2016-03-10 08:03:18 -0600

    primerasa dos carpetas

--- debug/fpe/test_fpe3.c
+++ debug/fpe/test_fpe3.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+//#include <cmath>
+#include <math.h>
 #ifdef TRAPFPE
-#include "fpe_x87_sse.h"
+#include "fpe_x87_sse/fpe_x87_sse.h"
 #endif
 
 int main(int argc, char *argv[])
